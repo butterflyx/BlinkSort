0879cbbcf7bcf216ba2fcf88378ff3d1c8b7020f
butterflyx <5388631+butterflyx@users.noreply.github.com>
2022-11-26 12:32:37 +0100

    [fix] function to find annotations

--- main.py
+++ main.py
@@ -15,65 +15,74 @@ log.basicConfig(encoding='utf-8', level=log.DEBUG)
 
 # "^(.+)$\n^(Kapitel (\d) :.+)$"
 # 
-# annotations = { booktitle, number_chapter , title_chapter , annotation }
+# annotations = { "booktitle" : booktitle, "chapters" : [{ number_chapter : {"chapter_title": chapter_title , "annotations" : [annotation_1 ,... annoation_n] }] }
 
 class BlinkSort:
-
-    text = ""
 
     textlist = []
 
     title = ""
 
-    chapter_names = {}
-    chapter_lines = []
+    chapters = {}
+
+    annotations = {}
 
     def __init__(self, filename):
         try:
-            with open(filename) as f:
-                self.text = f.read()
-                log.debug(f"fetched file {filename} with content:\n{self.text}")
+            with open(filename) as fp:
+                intext = fp.read()
+                log.debug(f"fetched file {filename} with content:\n{intext}")
+                self.textlist = intext.strip().split("\n")
+                self.textlist.reverse() # for chronological order
+                log.debug(f"converted intext into list:\n{self.textlist}")
         except FileNotFoundError as e:
             log.error(f"file {filename} not found: {e} \n exiting...")
             exit()
 
-        self.textlist = self.text.split('\n')
         self.set_title()
+        self.find_annotations()
+        
 
     def set_title(self) -> str:
-        self.title = self.textlist[0]
+        self.title = self.textlist[-1]
+        self.textlist.pop(-1) # remove the headline so the rest of the text is all the same structure
+        log.debug(f"fetched title '{self.title}' from last line. Textlist content is now:\n{self.textlist}")
+        self.annotations["booktitle"] = self.title
         return self.title
 
     def get_title(self) -> str:
         return self.title
 
-    def find_chapter_names(self) -> dict:
-        log.debug(f"start function to find chapters in text")
-        pattern = re.compile("Kapitel (\d) : (.+)")
-        for match in pattern.finditer(self.text):
-            self.chapter_names[int(match.group(1))] = match.group(2)
-        return self.chapter_names
-
-    def find_chapter_lines(self) -> list:
-        for index, line in enumerate(self.textlist):
-            if re.fullmatch(r"Kapitel (\d) : .+", line):
-                log.debug(f"found match for chapter in line {index}")
-                self.chapter_lines.append(index)
-        return self.chapter_lines
-
-    def find_annotations(self):
+    def find_annotations(self) -> dict:
         log.debug(f"start function to find annotations in text")
-        if (len(self.chapter_lines) == 0):
-            self.find_chapter_lines()
-        index_list = self.chapter_lines
-        index_list.insert(0, 0)
-        index = -1
-        while index > -(len(index_list)):
-            number_of_annotations = (index_list[index]-index_list[index-1])-1
-            log.debug(f"found {number_of_annotations} annotations between line {index_list[index]} and line {index_list[index-1]}")
-            for line in range(number_of_annotations):
-                log.debug(f"annoation: {self.textlist[index_list[index-1]+1]}")
-            index -= 1
+        chapter_index = 0
+        pattern = re.compile("Kapitel (\d) : (.+)")
+        for line in self.textlist:
+            log.debug(f"working on line:\n{line}")
+            match = re.search(pattern, line)
+            if match != None: # chapter found
+                log.debug(f"chapter found")
+                chapter_index = int(match.group(1))
+                log.debug(f"chapter index is now {chapter_index}")
+                if chapter_index not in self.chapters: # first match sets title
+                    self.chapters[chapter_index] = {"chapter_title" : match.group(2)}
+                    log.debug(f"chapter title is yet empty so set to '{match.group(2)}'")
+            elif chapter_index > 0: # annotation found
+                log.debug(f"annotation found: {line}")
+                if "annotations" not in self.chapters[chapter_index]: # no annotions yet
+                    log.debug(f"no annotions yet, so init key 'annotations' with empty list")
+                    self.chapters[chapter_index]["annotations"] = []
+                log.debug(f"append line to annotations")
+                self.chapters[chapter_index]["annotations"].append(line)
+            else:
+                raise FileStructureMissmatch
+        log.debug(f"done finding annotations.\n{self.chapters}")
+        log.debug(f"merge found annotations into annotations dict with book title")
+        self.annotations["chapters"] = self.chapters
+        return self.annotations
+    
+    def get_annotations(self) -> dict:
+        return self.annotations
         
 
         
@@ -82,6 +91,4 @@ class BlinkSort:
 if __name__ == "__main__":
     inputfile = "./data/demo.txt"
     blink = BlinkSort(inputfile)
-    blink.find_chapter_names()
-    print(blink.find_chapter_lines())
-    blink.find_annotations()
+    print(blink.get_annotations())
