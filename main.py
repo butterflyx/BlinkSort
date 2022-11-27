982a9ca28c9568629266f90801ae0bda85252406
butterflyx <5388631+butterflyx@users.noreply.github.com>
2022-11-27 18:47:12 +0100

    [fix] added choices in args as list

--- main.py
+++ main.py
@@ -13,14 +13,16 @@ import logging as log
 
 import lib.BlinkSort as BlinkSort
 
-
 if __name__ == "__main__":
+
+    output_options = ["JSON", "markdown"]
+
     parser = argparse.ArgumentParser(prog='main')
-    parser.add_argument('-f','--infile', required=True, type=argparse.FileType('r'), default=sys.stdin, help='path to file with raw text')
-    parser.add_argument('-i', '--indent', type=int, help='INDENT chars for JSON output', default=4)
+    parser.add_argument('-i','--infile', required=True, type=argparse.FileType('r'), default=sys.stdin, help='path to file with raw text')
+    parser.add_argument('-j', '--indent', type=int, help='INDENT chars for JSON output', default=4)
     parser.add_argument('-c','--cite', action='store_true', help='format markdown output as citations')
     parser.add_argument('-v', '--verbose', action='store_true', help='show debugging information')
-    parser.add_argument('-o', '--output_format', action='store', required=True, help="specify output format: J = JSON or M = Markdown")
+    parser.add_argument('-f', '--output_format', action='store', required=True, choices=output_options, help="specify output format")
     args = parser.parse_args()
 
     log.basicConfig(encoding='utf-8', level=log.DEBUG) if args.verbose else log.basicConfig(encoding='utf-8', level=log.ERROR)
@@ -32,10 +34,10 @@ if __name__ == "__main__":
     except FileNotFoundError as e:
         exit(f"File '{inputfile}' not found: {e}")
 
-    if args.output_format == "M":
+    if args.output_format == "markdown":
         log.debug(f"output should be formated as markdown")
         output = blink.output_markdown() if args.cite else blink.output_markdown(cite=False)            
-    elif args.output_format == "J":
+    elif args.output_format == "JSON":
         log.debug(f"output should be formated as JSON")
         output = blink.output_json(indent=args.indent)
     else:
